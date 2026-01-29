f918a1c7505a70bfa99f3998874dddcc3c5a6c9a
Tiara <tiara03study@gmail.com>
2026-01-30 00:00:36 +0800

    saya hapus satu file

--- manajemen_materi/domain/entities/entities.py
+++
@@ -1,9 +0,0 @@
-class Materi:
-    def __init__(self, id, matakuliah, judul, deskripsi, tipe, file, link):
-        self.id = id
-        self.matakuliah = matakuliah
-        self.judul = judul
-        self.deskripsi = deskripsi
-        self.tipe = tipe      # file | link | video
-        self.file = file
-        self.link = link
